59998726be73b495e8474f47913efa3cc8a0e83c
Johannus V <johannus.vogel@gmail.com>
2016-08-02 17:13:09 +0200

    fixed syntax bug from last commit

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
-  has_many :video_categories, :dependent => :destroy_all
-  has_many :user_categories, :dependent => :destroy_all
+  has_many :video_categories, :dependent => :destroy
+  has_many :user_categories, :dependent => :destroy
   has_many :videos, :through => :video_categories
   has_many :users, :through => :user_categories
   has_attached_file :category_image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
