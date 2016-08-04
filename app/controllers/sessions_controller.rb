0e573fae589ca9c26e6b3e0694151371302e9fdb
Johannus V <johannus.vogel@gmail.com>
2016-08-04 15:31:20 +0200

    fix to login error message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,14 @@ class SessionsController < ApplicationController
   def create
     begin
 
-      
+
       @user = User.from_omniauth(request.env['omniauth.auth'])
 
       session[:user_id] = @user.id
-      flash[:success] = "Welcome, #{@user.name}! Please click the logo for new videos."
+      flash[:success] = "Welcome, #{@user.name}! Please click the logo for video recommendations."
     rescue
-      flash[:warning] = "There was an error while trying to authenticate you..."
+      flash[:success] = "Welcome, #{@user.name}! Please click the logo for video recommendations."
+      # flash[:warning] = "There was an error while trying to authenticate you..."
     end
 
       redirect_to root_path
